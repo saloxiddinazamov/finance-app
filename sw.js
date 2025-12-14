577d0e2fd891e431cb2e04232a6986d06546687b
saloxiddinazamov <pdguzb@gmail.com>
2025-12-15 02:19:38 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "finance-pwa-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+});
